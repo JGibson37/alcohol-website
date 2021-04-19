5ba3a958cb715ca498922f472ab4cc14474154da
Jason Gibson <65173587+JGibson37@users.noreply.github.com>
2021-04-19 14:46:08 -0400

    added some text and styling to homepage

--- src/App.js
+++ src/App.js
@@ -6,7 +6,35 @@ import CocktailSearch from "./components/CocktailSearch";
 const views = ["home", "random-drink", "ingredient-search", "cocktail-search"];
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <div>
+      <h1 className="flex justify-center text-5xl font-bold">Alcohol Search</h1>
+      <br></br>
+      <p>
+        Welcome to my small search engine I built. If you're feeling lucky try
+        Random-Drink. If you want to know more about an ingredient, try
+        Ingredient-Search. If you want to find a specific drink, try
+        Cocktail-Search. Searching can be a little hard if you don't spell your
+        drink or ingredient exactly correct. As such I've provided you with a
+        few example searches so you can explore the functionality yourself.
+      </p>
+      <br></br>
+      <div className="grid gap-y-4" style={{ gridTemplaterows: "auto auto" }}>
+        <p className="text-lg font-bold">Ingredient-Search Examples:</p>
+        <ul>
+          <li>Gin</li>
+          <li>Vodka</li>
+          <li>Whiskey</li>
+        </ul>
+        <p className="text-lg font-bold">Cocktail-Search Examples:</p>
+        <ul>
+          <li>Absolut Summertime</li>
+          <li>Atlantic Sun</li>
+          <li>Moranguito</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 function App() {
